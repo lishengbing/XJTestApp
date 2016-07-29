c11e577403ffb4e3c702f89745a8acfb72be359d
lishengbing <1269113083@qq.com>
2016-07-29 14:05:28 +0800

    初始化项目

---
+++ XJTestApp/XJTestApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XJTestApp
+//
+//  Created by 李胜兵 on 16/7/29.
+//  Copyright © 2016年 李胜兵. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
